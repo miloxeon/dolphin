ae0544af9b8725bf095f699c09fb9d466bb87e34
uyouthe <melvoloskov@ya.ru>
2017-05-25 00:38:56 +0300

    everything is done for rewriting the app

--- blueprints/stagezero/connections/style.js
+++ blueprints/stagezero/connections/style.js
@@ -2,12 +2,28 @@
 
 // connection line styling
 
+// 	line_style:
+// 		stroke
+// 		stroke-width
+// 		fill
+// 		stroke-linecap
+// 		stroke-dasharray
+
+
 import {fillObject} from '../tools';
 
-export function convertLineStyle(style) {
+export function convertLineStyle(line_style) {
 	var default_line_style = {
-		
+		'stroke': 'black',
+		'stroke-width': '2',
+		'fill': 'none',
+		'stroke-linecap': 'round',
+		'stroke-dasharray': 'none'
 	}
 
-	
+	var passed_style = fillObject(line_style, default_line_style);
+
+	return {
+		line_style: passed_style
+	}
 }
