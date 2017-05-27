3837290c0c7ec0ac6869addf1f2a757dbd23ce24
Милослав Волосков <melvoloskov@ya.ru>
2017-05-27 18:43:47 +0300

    element haven't ever been better

--- stagezero/src/lib/element/text.js
+++ stagezero/src/lib/element/text.js
@@ -2,33 +2,133 @@
 
 // element text processing
 
-export function prepareText(element_text) {
+var merge = require('deepmerge');
 
-	// split text to something like `+ variable : int = 0;`, which is used in UML
-	
-	var prepared_attributes = element_text.attributes.map(function (attribute) {
+export function addAttributes(element, text, style) {
+	// + foo : int = "bar"
+	// 
+	let attr_style = style.attribute;
 
-		return [
-			getScopeSymbol(attribute.scope),
-			attribute.name,
-			(attribute.type === 'any') ? '' : ': ' + attribute.type
-		].join(' ') + 
-			((attribute.value !== '') ? [' =', attribute.value].join(' ') : '');
-	})
+	return element.text(function (add) {
+		for (let name in text) {
+			let value = text[name];
 
-	var prepared_methods = element_text.methods.map(function (method) {
-		return [
-			getScopeSymbol(method.scope),
-			(method.type === 'any') ? '' : method.type,
-			method.name + '()'
-		].join(' ');
-	})
+			add.tspan(getScopeSymbol(value.scope)).font(attr_style.scope).newLine();
 
-	return {
-		name: element_text.name,
-		attributes: prepared_attributes.join('\n'),
-		methods: prepared_methods.join('\n')
-	}
+			add.tspan(' ');
+
+			add.tspan(value.name + ' ').font(attr_style.name);
+
+			if (value.type !== 'any') {
+				add.tspan(' : ');
+				add.tspan(value.type + ' ').font(attr_style.type);
+			}
+
+
+			if (value.value) {
+
+				add.tspan('= ').font(attr_style.symbol);
+
+				if (value.type === 'string') {
+
+					add.tspan('"' + value.value + '"').font(merge(attr_style.value.common, attr_style.value.string));
+
+				} else if (value.type === 'int') {
+
+					add.tspan(value.value).font(merge(attr_style.value.common, attr_style.value.integer));
+
+				} else {
+
+					add.tspan(value.value).font(attr_style.value.common);
+				}
+			}
+		}
+	}).font(attr_style.common);
+}
+
+export function addMethods(element, text, style) {
+	// - string getFoo(
+	// 		bar: int,
+	// 		foo: string = "hello")
+	let method_style = style.method;
+	let attr_style = style.attribute;
+
+	return element.text(function (add) {
+		for (let name in text) {
+			let value = text[name];
+
+			add.tspan(getScopeSymbol(value.scope)).font(method_style.scope).newLine();
+
+			add.tspan(' ');
+
+			if (value.type !== 'any') {
+				add.tspan(value.type + ' ').font(method_style.type);
+			}
+
+			add.tspan(value.name + ' ').font(method_style.name);
+
+			if (value.args) {
+
+				add.tspan('(').font(method_style.name);
+
+				for (let arg in value.args) {
+
+					let argument = value.args[arg];
+					let arg_style = attr_style;
+
+					add.tspan(argument.name)
+						.font(arg_style.name)
+						.font(method_style.passed)
+						.newLine()
+						.dx(20);
+					
+					if (argument.type !== 'any') {
+						add.tspan(' : ').font(method_style.passed);
+						add.tspan(argument.type)
+							.font(arg_style.type)
+							.font(method_style.passed);
+					}
+
+					if (argument.value !== '') {
+						add.tspan(' ').font(method_style.passed);;
+						add.tspan('=').font(arg_style.symbol).font(method_style.passed);
+						add.tspan(' ').font(method_style.passed);;
+
+						if (argument.type === 'string') {
+
+							add.tspan('"' + argument.value + '"')
+								.font(merge(arg_style.value.common, arg_style.value.string))
+								.font(method_style.passed);
+
+						} else if (argument.type === 'int') {
+
+							add.tspan(argument.value)
+								.font(merge(arg_style.value.common, arg_style.value.integer))
+								.font(method_style.passed);
+
+						} else {
+
+							add.tspan(argument.value)
+								.font(arg_style.value.common)
+								.font(method_style.passed);
+						}
+
+					}
+
+					if (arg < value.args.length - 1) {
+						add.tspan(', ')
+							.font(arg_style.common)
+							.font(method_style.passed);
+					} else {
+						add.tspan(')').font(method_style.name);
+					}
+				}
+
+			} else {
+				add.tspan('()').font(method_style.name);
+			}
+		}
+	}).font(method_style.common);
 }
 
 function getScopeSymbol(scope) {
---
+++ stagezero/src/lib/theme/model.js
@@ -0,0 +1,87 @@
+'use strict';
+
+export let class_theme = {
+	rect_style: {
+		'padding': '11 11',
+		'border-color': 'black',
+		'border-width': '2',
+		'border-radius': '4',
+		'background-color': 'white'
+	},
+	text_style: {
+		common: {
+			// styles applied to overall text
+			'color': 'black',
+			'font-family': 'Verdana, sans-serif',
+			'font-size': '12',
+			'line-height': '1.25',
+			'font-style': 'normal',
+			'font-weight': 'normal',
+			'text-align': 'left'
+		},
+		node: {
+			common: {
+			},
+			name: {
+				'font-weight': 'bold'
+			},
+			type: {
+				// for example, <<interface>>
+				'font-style': 'italic',
+				'font-size': '9'
+			}
+		},
+		attribute: {
+			common: {
+				// styles applied to overall attributes
+			},
+			scope: {
+				// +, -, /, ~ and so on
+				'font-family': 'Courier New, monospace',
+				'color': '#B90690'
+			},
+			name: {
+
+			},
+			type: {
+				'color': 'blue',
+				'font-style': 'italic'
+			},
+			symbol: {
+
+			},
+			value: {
+				common: {
+					'color': 'blue'
+				},
+				string: {
+					'color': '#036A07'
+				},
+				integer: {
+
+				}
+			}
+		},
+		method: {
+			common: {
+				
+			},
+			scope: {
+				'font-family': 'Courier New, monospace',
+				'color': '#B90690'
+			},
+			type: {
+				'color': 'blue'
+			},
+			name: {
+				'color': '#00002A',
+			},
+			passed: {
+				// 'color': 'dimgrey'
+			}
+		}
+	},
+	line_style: {
+		
+	}
+};
