a4962ddfd45bf70a7925d6bacbd6031cedc29c17
Милослав Волосков <melvoloskov@ya.ru>
2017-05-31 00:22:32 +0300

    arrow text

--- production/frontend/editor/src/lib/connection/index.js
+++ production/frontend/editor/src/lib/connection/index.js
@@ -88,20 +88,22 @@ export function redraw() {
 		socket: sockets[1]
 	}
 
+	let type = this.blueprint.type;
+
 	let connector = getLineFunction(sockets);
-	this.connectSockets(from, to, connector);
+	this.connectSockets(from, to, type, connector);
 }
 
-export function connectSockets (from, to, connector = cubicTo) {
+export function connectSockets (from, to, type, connector = cubicTo) {
 	// connect two elements' sockets (abstraction over connection by two coordinates)
 	let from_dot = from.element.socket(from.socket);
 	let to_dot = to.element.socket(to.socket);
 	let isReverse = from_dot.x > to_dot.x;
 
 	if (!isReverse) {
-		this.connectDots(from_dot, to_dot, connector, false);
+		this.connectDots(from_dot, to_dot, type, connector, false);
 	} else {
-		this.connectDots(to_dot, from_dot, connector, true);
+		this.connectDots(to_dot, from_dot, type, connector, true);
 	}
 
 	return this;
@@ -124,10 +126,10 @@ function computeOffset(part, all, position) {
 
 		case 'middle':
 			// magic
-			return (50 - ((part.bbox().w / path_length) * 50)) + '%';
+			return (50 - ((part / path_length) * 50)) + '%';
 
 		case 'end':
-			return path_length - part.bbox().w - padding;
+			return path_length - part - padding;
 
 		default:
 			// err
@@ -142,65 +144,83 @@ export function displayLineText(isReverse = false) {
 
 
 	// text at the middle
-	let line_text = this.text(function (add) { add.tspan(richText.text).dy(offset_upper)})
-		.addClass('dolphin_text dolphin_line_text dolphin_line_action')
-		.path(path.array())
-		.textPath();
-	line_text.attr({
-		'startOffset': computeOffset(line_text, path, 'middle'),
-		'spacing': 'auto'
-	});
+	// let line_text = this.text(function (add) { add.tspan(richText.text).dy(offset_upper)})
+	// 	.addClass('dolphin_text dolphin_line_text dolphin_line_action')
+	// 	.path(path.array())
+	// 	.textPath();
+	// line_text.attr({
+	// 	'startOffset': computeOffset(line_text, path, 'middle'),
+	// 	'spacing': 'auto'
+	// });
 	
 	// role at the start
-	let role_start = this.text(function (add) { add.tspan(richText.from.role).dy(offset_upper)})
-		.addClass('dolphin_text dolphin_line_text dolphin_line_role')
+	let role_start_width;
+	let role_start = this.text(function (add) {
+
+		let t = add.tspan(richText.from.role).dy(offset_upper);
+		role_start_width = t.bbox().w;
+
+	}).addClass('dolphin_text dolphin_line_text dolphin_line_role')
 		.path(path.array())
 		.textPath();
 	role_start.attr({
-		'startOffset': computeOffset(role_start, path, !isReverse ? 'start' : 'end'),
-		'spacing': 'auto',
-		'anchor': !isReverse ? 'start' : 'end'
+		'startOffset': computeOffset(role_start_width, path, !isReverse ? 'start' : 'end'),
+		'spacing': 'auto'
 	});
 	
 	// indicator at the start
-	let indicator_start = this.text(function (add) { add.tspan(richText.from.indicator).dy(offset_lower)})
-		.addClass('dolphin_text dolphin_line_text dolphin_line_indicator')
+	let indicator_start_width;
+	let indicator_start = this.text(function (add) {
+
+		let t = add.tspan(richText.from.indicator).dy(offset_lower);
+		indicator_start_width = t.bbox().w;
+
+	}).addClass('dolphin_text dolphin_line_text dolphin_line_indicator')
 		.path(path.array())
 		.textPath();
 	indicator_start.attr({
-		'startOffset': computeOffset(indicator_start, path, !isReverse ? 'start' : 'end'),
-		'spacing': 'auto',
-		'anchor': !isReverse ? 'start' : 'end'
+		'startOffset': computeOffset(indicator_start_width, path, !isReverse ? 'start' : 'end'),
+		'spacing': 'auto'
 	});
 	
 	// role at the end
-	let role_end = this.text(function (add) { add.tspan(richText.to.role).dy(offset_upper)})
-		.addClass('dolphin_text dolphin_line_text dolphin_line_role')
+	let role_end_width;
+	let role_end = this.text(function (add) {
+
+		let t = add.tspan(richText.to.role).dy(offset_upper);
+		role_end_width = t.bbox().w;
+
+	}).addClass('dolphin_text dolphin_line_text dolphin_line_role')
 		.path(path.array())
 		.textPath();
 	role_end.attr({
-		'startOffset': computeOffset(role_end, path, !isReverse ? 'end' : 'start'),
-		'spacing': 'auto',
-		'anchor': !isReverse ? 'end' : 'start'
+		'startOffset': computeOffset(role_end_width, path, !isReverse ? 'end' : 'start'),
+		'spacing': 'auto'
 	});
-	
+
 	// indicator at the end
-	let indicator_end = this.text(function (add) { add.tspan(richText.to.indicator).dy(offset_lower)})
-		.addClass('dolphin_text dolphin_line_text dolphin_line_indicator')
+	let indicator_end_width;
+	let indicator_end = this.text(function (add) {
+
+		let t = add.tspan(richText.to.indicator).dy(offset_lower);
+		indicator_end_width = t.bbox().w;
+
+	}).addClass('dolphin_text dolphin_line_text dolphin_line_indicator')
 		.path(path.array())
 		.textPath();
 	indicator_end.attr({
-		'startOffset': computeOffset(indicator_end, path, !isReverse ? 'end' : 'start'),
-		'spacing': 'auto',
-		'anchor': !isReverse ? 'end' : 'start'
+		'startOffset': computeOffset(indicator_end_width, path, !isReverse ? 'end' : 'start'),
+		'spacing': 'auto'
 	});
 		
 	return this;
 }
 
-export function connectDots (a, b, connector, isReverse) {
+export function connectDots (a, b, type, connector, isReverse) {
 	// connect two coordinates with a line
 	let id = getRawId(this.id());
+
+	console.log(type);
 
 	this.connectionLine = this.path(connector(a, b))
 		.attr('id', getId(id))
