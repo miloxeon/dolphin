a4f2b9836ab91575111fa27f4055ddb42a90f8c9
Милослав Волосков <melvoloskov@ya.ru>
2017-06-06 10:00:47 +0300

    new config

--- frontend/src/editor/index.js
+++ frontend/src/editor/index.js
@@ -4,109 +4,47 @@
 // todo errors
 
 require('./css/style.css');
-let Gun = require('gun');
-import {createStore} from 'redux';
 import {draw} from './lib/classes';
 import {moveController} from './lib/controllers';
 import {clone} from './lib/tools';
 import {addElement, removeElement, addConnection, removeConnection, move} from './actions';
 
+let Gun = require('gun');
+let peers = ['http://localhost:8080/gun'];
+let gun = Gun(peers);
 
-var peers = ['http://localhost:8080/gun'];
-var gun = Gun(peers);
-var storage = gun.get('model');
+let storage = gun.get('model');
 
-var store;
-var diagram = draw.classDiagram();
+let model;
+let diagram;
 
 storage.val(function (data) {
-	store = createStore(modelReducer, JSON.parse(data.fixtures));
+	diagram = draw.classDiagram();
+	model = JSON.parse(data.fixtures);
 
 	build();
 
 	diagram.on('redraw', diagram.redrawConnections);
-	diagram.on('rebuild', function (e) {
-		store.dispatch({
-			type: 'MOVE',
-			payload: e.detail.node
-		});
+	diagram.on('rebuild', function (event) {
+		storage.get('fixtures').put(JSON.stringify(event.detail.new_model));
 	});
+});
 
-	store.subscribe(() => {
-		rebuild();
-		storage.get('fixtures').put(JSON.stringify(store.getState()));
-	})
+storage.on(function (data) {
+	model = JSON.parse(data.fixtures);
+	rebuild();
 });
 
 module.exports = {
-	addElement: _addElement,
-	removeElement: _removeElement,
-	addConnection: _addConnection,
-	removeConnection: _removeConnection,
+	addElement,
+	removeElement,
+	addConnection,
+	removeConnection,
 	getModel
-}
- 
-function _addElement(blueprint) {
-	store.dispatch({
-		type: 'ADD_ELEMENT',
-		payload: blueprint
-	});
-}
-
-function _removeElement(id) {
-	store.dispatch({
-		type: 'REMOVE_ELEMENT',
-		payload: id
-	});
-}
-
-function _addConnection(blueprint) {
-	store.dispatch({
-		type: 'ADD_CONNECTION',
-		payload: blueprint
-	});
-}
-
-function _removeConnection(id) {
-	store.dispatch({
-		type: 'REMOVE_CONNECTION',
-		payload: id
-	});
-}
-
-
-function modelReducer(state, action) {
-	switch (action.type) {
-
-		case 'ADD_ELEMENT':
-			return addElement(state, action.payload);
-
-		case 'REMOVE_ELEMENT':
-			return removeElement(state, action.payload);
-
-		case 'ADD_CONNECTION':
-			return addConnection(state, action.payload);
-
-		case 'REMOVE_CONNECTION':
-			return removeConnection(state, action.payload);
-
-		case 'MOVE':
-			return move(state, action.payload);
-
-		case 'REPLACE':
-			return replaceStore(state, action.payload)
-
-		default:
-			return state;
-	}
-}
-
-function replaceStore(state, action) {
-	return clone(action);
 }
 
 function getModel () {
-	return store.getState();
+	return clone(model);
 }
 
 function bindControllers(diagram) {
@@ -119,14 +57,21 @@ function bindControllers(diagram) {
 		child.on('dragend', function () {
 
 			diagram.fire('rebuild', {
-				node: this
+				new_model: move(model, this)
 			});
 		});
 	});
 }
 
+function unbindControllers(diagram) {
+	diagram.children().forEach(function (child) {
+		child.off();
+	});
+}
+
 function build() {
-	diagram.fromModel(store.getState());
+	unbindControllers(diagram);
+	diagram.fromModel(model);
 	bindControllers(diagram);
 }
 
